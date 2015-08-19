e5b4081698d18327dc52621216c82382bc7b9364
everright.chen <everright.chen@gmail.com>
2015-08-19 12:34:55 +0800

    Fix js coding format.

--- js/cloudinary.js
+++ js/cloudinary.js
@@ -5,16 +5,18 @@
 
 (function ($) {
 
+  "use strict";
+
   Drupal.behaviors.cloudinary = Drupal.behaviors.cloudinary || {};
 
-  Drupal.behaviors.cloudinary.attach = function(context) {
+  Drupal.behaviors.cloudinary.attach = function (context) {
     Drupal.cloudinary.ui(context);
   };
 
   Drupal.cloudinary = Drupal.cloudinary || {};
 
-  Drupal.cloudinary.ui = function(context) {
-    var farb_change_color = function(obj, color) {
+  Drupal.cloudinary.ui = function (context) {
+    var farb_change_color = function (obj, color) {
       if (!color) {
         color = '#000';
       }
@@ -31,32 +33,32 @@
       .once('farbtastic');
     var farb = $.farbtastic($picker);
     // Bind input as color picker.
-    $('.input_color', context).once('input_color', function() {
+    $('.input_color', context).once('input_color', function () {
       var $ic = $(this);
       farb_change_color($ic, $ic.val());
-      $ic.focus(function() {
-        var position = $ic.position(),
-          left = position.left - ((195 - $ic.width()) / 2),
-          top = position.top - 195;
+      $ic.focus(function () {
+        var position = $ic.position();
+        var left = position.left - ((195 - $ic.width()) / 2);
+        var top = position.top - 195;
 
-        farb.linkTo(function(color) {
+        farb.linkTo(function (color) {
           farb_change_color($ic.val(color), color);
         }).setColor($ic.val());
 
         $picker.css({'left': left, 'top': top}).fadeIn();
-      }).focusout(function() {
+      }).focusout(function () {
         farb.setColor($ic.val());
         $picker.hide();
       });
     });
     // Bind textfield as jquery ui slider.
-    $('.input_slider', context).once('input_slider', function() {
-      var $select = $(this),
-        min = 0,
-        max = 200,
-        fixed = 'dynamic',
-        css = 'slider',
-        data = $select.attr('data');
+    $('.input_slider', context).once('input_slider', function () {
+      var $select = $(this);
+      var min = 0;
+      var max = 200;
+      var fixed = 'dynamic';
+      var css = 'slider';
+      var data = $select.attr('data');
       // Assign data if get custom value.
       if (data) {
         var datas = data.split('_', 4);
@@ -75,37 +77,37 @@
         max: max,
         range: 'min',
         value: parseInt($select.val()) || 0,
-        slide: function(event, ui) {
+        slide: function (event, ui) {
           $select.val(ui.value);
         }
       });
       // Update slider max if input value large.
       // Update value with min if less than min value.
-      $select.change(function() {
+      $select.change(function () {
         var current_value = parseInt($(this).val());
         if (current_value < min) {
           current_value = min;
         }
-        else if ('fixed' == fixed && current_value > max) {
+        else if (fixed === 'fixed' && current_value > max) {
           current_value = max;
         }
         $(this).val(current_value);
         $slider.slider('value', current_value);
-        if ('fixed' != fixed && current_value > max) {
+        if (fixed !== 'fixed' && current_value > max) {
           $slider.slider('option', 'max', current_value);
         }
       });
     });
     // Bind reset button.
-    $('#edit-reset').click(function() {
-      $(this).parents('form').map(function() {
+    $('#edit-reset').click(function () {
+      $(this).parents('form').map(function () {
         this.reset();
         var $input = $(this).find(':input');
         $input.not('.input_slider').change();
-        $input.filter('.input_color').map(function() {
+        $input.filter('.input_color').map(function () {
           farb_change_color($(this), $(this).val());
         });
-        $input.filter('.input_slider').map(function() {
+        $input.filter('.input_slider').map(function () {
           var cv = $(this).val();
           $(this).change().val(cv);
         });
